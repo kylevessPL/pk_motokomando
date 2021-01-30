99acd253ead1310c4bd35050c77f71402bed4578
Kacper <222468@edu.p.lodz.pl>
2021-01-30 18:41:10 +0100

    anchorpane's variables names changed

--- application/desktop-application-parent/desktop-application-view/src/main/java/pl/motokomando/healthcare/view/base/BaseView.java
+++ application/desktop-application-parent/desktop-application-view/src/main/java/pl/motokomando/healthcare/view/base/BaseView.java
@@ -28,14 +28,14 @@ public class BaseView {
     private Tab tab_doctors;
     private TabPane tabPane_doctors;
     private Tab tab_find_doctor;
-    private AnchorPane anchorPane;
+    private AnchorPane anchorPane_find_doctor;
     private TableView<DoctorRecord> tableView_doctors;
     private TableColumn<DoctorRecord, String> tableColumn_doctor_name;
     private TableColumn<DoctorRecord, String> tableColumn_doctor_surname;
     private TableColumn<DoctorRecord, String> tableColumn_doctor_phone_number;
     private TableColumn<DoctorRecord, String> tableColumn_doctor_specialisation;
     private Tab tab_add_new_doctor;
-    private AnchorPane anchorPane0;
+    private AnchorPane anchorPane_add_new_doctor;
     private TextField textField_new_doctor_name;
     private Button button_add_new_doctor;
     private TextField textField_new_doctor_surname;
@@ -45,7 +45,7 @@ public class BaseView {
     private Tab tab_patients;
     private TabPane tabPane_patients;
     private Tab tab_find_patient;
-    private AnchorPane anchorPane1;
+    private AnchorPane anchorPane_find_patient;
     private TableView<PatientRecord> tableView_patients;
     private TableColumn<PatientRecord, String> tableColumn_patient_name;
     private TableColumn<PatientRecord, String> tableColumn_patient_surname;
@@ -53,7 +53,7 @@ public class BaseView {
     private TableColumn<PatientRecord, String> tableColumn_patient_blood_group;
     private TableColumn<PatientRecord, String> tableColumn_patient_id_number;
     private Tab tab_add_new_patient;
-    private AnchorPane anchorPane2;
+    private AnchorPane anchorPane_add_new_patient;
     private TextField textField_new_patient_name;
     private TextField textField_new_patient_surname;
     private DatePicker datePicker_new_patient_birthday;
@@ -96,14 +96,14 @@ public class BaseView {
         tab_doctors = new Tab();
         tabPane_doctors = new TabPane();
         tab_find_doctor = new Tab();
-        anchorPane = new AnchorPane();
+        anchorPane_find_doctor = new AnchorPane();
         tableView_doctors = new TableView<>();
         tableColumn_doctor_name = new TableColumn<>();
         tableColumn_doctor_surname = new TableColumn<>();
         tableColumn_doctor_phone_number = new TableColumn<>();
         tableColumn_doctor_specialisation = new TableColumn<>();
         tab_add_new_doctor = new Tab();
-        anchorPane0 = new AnchorPane();
+        anchorPane_add_new_doctor = new AnchorPane();
         textField_new_doctor_name = new TextField();
         button_add_new_doctor = new Button();
         textField_new_doctor_surname = new TextField();
@@ -113,7 +113,7 @@ public class BaseView {
         tab_patients = new Tab();
         tabPane_patients = new TabPane();
         tab_find_patient = new Tab();
-        anchorPane1 = new AnchorPane();
+        anchorPane_find_patient = new AnchorPane();
         tableView_patients = new TableView<>();
         tableColumn_patient_name = new TableColumn<>();
         tableColumn_patient_surname = new TableColumn<>();
@@ -121,7 +121,7 @@ public class BaseView {
         tableColumn_patient_blood_group = new TableColumn<>();
         tableColumn_patient_id_number = new TableColumn<>();
         tab_add_new_patient = new Tab();
-        anchorPane2 = new AnchorPane();
+        anchorPane_add_new_patient = new AnchorPane();
         textField_new_patient_name = new TextField();
         textField_new_patient_surname = new TextField();
         datePicker_new_patient_birthday = new DatePicker();
@@ -145,10 +145,10 @@ public class BaseView {
 
         tab_find_doctor.setText("Znajdź doktora");
 
-        anchorPane.setMinHeight(0.0);
-        anchorPane.setMinWidth(0.0);
-        anchorPane.setPrefHeight(180.0);
-        anchorPane.setPrefWidth(200.0);
+        anchorPane_find_doctor.setMinHeight(0.0);
+        anchorPane_find_doctor.setMinWidth(0.0);
+        anchorPane_find_doctor.setPrefHeight(180.0);
+        anchorPane_find_doctor.setPrefWidth(200.0);
 
         tableView_doctors.setLayoutX(50.0);
         tableView_doctors.setLayoutY(50.0);
@@ -168,14 +168,14 @@ public class BaseView {
         tableColumn_doctor_specialisation.setPrefWidth(375.0);
         tableColumn_doctor_specialisation.setText("Specjalizacja");
 
-        tab_find_doctor.setContent(anchorPane);
+        tab_find_doctor.setContent(anchorPane_find_doctor);
 
         tab_add_new_doctor.setText("Dodaj Doktora");
 
-        anchorPane0.setMinHeight(0.0);
-        anchorPane0.setMinWidth(0.0);
-        anchorPane0.setPrefHeight(800.0);
-        anchorPane0.setPrefWidth(1600.0);
+        anchorPane_add_new_doctor.setMinHeight(0.0);
+        anchorPane_add_new_doctor.setMinWidth(0.0);
+        anchorPane_add_new_doctor.setPrefHeight(800.0);
+        anchorPane_add_new_doctor.setPrefWidth(1600.0);
 
         textField_new_doctor_name.setLayoutX(650.0);
         textField_new_doctor_name.setLayoutY(50.0);
@@ -213,7 +213,7 @@ public class BaseView {
         label_new_doctor_specialisation.setLayoutY(340.0);
         label_new_doctor_specialisation.setText("Specjalizacja:");
         label_new_doctor_specialisation.setFont(new Font(16.0));
-        tab_add_new_doctor.setContent(anchorPane0);
+        tab_add_new_doctor.setContent(anchorPane_add_new_doctor);
         tab_doctors.setContent(tabPane_doctors);
 
         tab_patients.setText("Pacjenci");
@@ -221,10 +221,10 @@ public class BaseView {
         tab_find_patient.setClosable(false);
         tab_find_patient.setText("Wyszukaj pacjenta");
 
-        anchorPane1.setMinHeight(0.0);
-        anchorPane1.setMinWidth(0.0);
-        anchorPane1.setPrefHeight(180.0);
-        anchorPane1.setPrefWidth(200.0);
+        anchorPane_find_patient.setMinHeight(0.0);
+        anchorPane_find_patient.setMinWidth(0.0);
+        anchorPane_find_patient.setPrefHeight(180.0);
+        anchorPane_find_patient.setPrefWidth(200.0);
 
         tableView_patients.setLayoutX(50.0);
         tableView_patients.setLayoutY(50.0);
@@ -247,15 +247,15 @@ public class BaseView {
         tableColumn_patient_id_number.setMinWidth(0.0);
         tableColumn_patient_id_number.setPrefWidth(300.0);
         tableColumn_patient_id_number.setText("Numer dokumentu");
-        tab_find_patient.setContent(anchorPane1);
+        tab_find_patient.setContent(anchorPane_find_patient);
 
         tab_add_new_patient.setClosable(false);
         tab_add_new_patient.setText("Dodaj pacjenta");
 
-        anchorPane2.setMinHeight(0.0);
-        anchorPane2.setMinWidth(0.0);
-        anchorPane2.setPrefHeight(180.0);
-        anchorPane2.setPrefWidth(200.0);
+        anchorPane_add_new_patient.setMinHeight(0.0);
+        anchorPane_add_new_patient.setMinWidth(0.0);
+        anchorPane_add_new_patient.setPrefHeight(180.0);
+        anchorPane_add_new_patient.setPrefWidth(200.0);
 
         textField_new_patient_name.setLayoutX(350.0);
         textField_new_patient_name.setLayoutY(50.0);
@@ -356,21 +356,21 @@ public class BaseView {
         label_new_patient_id_type.setLayoutY(340.0);
         label_new_patient_id_type.setText("Dokument:");
         label_new_patient_id_type.setFont(new Font(16.0));
-        tab_add_new_patient.setContent(anchorPane2);
+        tab_add_new_patient.setContent(anchorPane_add_new_patient);
         tab_patients.setContent(tabPane_patients);
 
         tableView_doctors.getColumns().add(tableColumn_doctor_name);
         tableView_doctors.getColumns().add(tableColumn_doctor_surname);
         tableView_doctors.getColumns().add(tableColumn_doctor_phone_number);
         tableView_doctors.getColumns().add(tableColumn_doctor_specialisation);
-        anchorPane.getChildren().add(tableView_doctors);
+        anchorPane_find_doctor.getChildren().add(tableView_doctors);
         tabPane_doctors.getTabs().add(tab_find_doctor);
-        anchorPane0.getChildren().add(textField_new_doctor_name);
-        anchorPane0.getChildren().add(button_add_new_doctor);
-        anchorPane0.getChildren().add(textField_new_doctor_surname);
-        anchorPane0.getChildren().add(textField_new_doctor_phone_number);
-        anchorPane0.getChildren().add(choiceBox_doctor_specialisations);
-        anchorPane0.getChildren().add(label_new_doctor_specialisation);
+        anchorPane_add_new_doctor.getChildren().add(textField_new_doctor_name);
+        anchorPane_add_new_doctor.getChildren().add(button_add_new_doctor);
+        anchorPane_add_new_doctor.getChildren().add(textField_new_doctor_surname);
+        anchorPane_add_new_doctor.getChildren().add(textField_new_doctor_phone_number);
+        anchorPane_add_new_doctor.getChildren().add(choiceBox_doctor_specialisations);
+        anchorPane_add_new_doctor.getChildren().add(label_new_doctor_specialisation);
         tabPane_doctors.getTabs().add(tab_add_new_doctor);
         mainPane.getTabs().add(tab_doctors);
         tableView_patients.getColumns().add(tableColumn_patient_name);
@@ -378,24 +378,24 @@ public class BaseView {
         tableView_patients.getColumns().add(tableColumn_patient_phone_number);
         tableView_patients.getColumns().add(tableColumn_patient_blood_group);
         tableView_patients.getColumns().add(tableColumn_patient_id_number);
-        anchorPane1.getChildren().add(tableView_patients);
+        anchorPane_find_patient.getChildren().add(tableView_patients);
         tabPane_patients.getTabs().add(tab_find_patient);
-        anchorPane2.getChildren().add(textField_new_patient_name);
-        anchorPane2.getChildren().add(textField_new_patient_surname);
-        anchorPane2.getChildren().add(datePicker_new_patient_birthday);
-        anchorPane2.getChildren().add(choiceBox_new_patient_sex);
-        anchorPane2.getChildren().add(choiceBox_new_patient_blood_group);
-        anchorPane2.getChildren().add(textField_new_patient_street);
-        anchorPane2.getChildren().add(textField_new_patient_house_number);
-        anchorPane2.getChildren().add(textField_new_patient_zip_code);
-        anchorPane2.getChildren().add(textField_new_patient_city);
-        anchorPane2.getChildren().add(choiceBox_new_patient_id_type);
-        anchorPane2.getChildren().add(textField_new_patient_id_number);
-        anchorPane2.getChildren().add(textField_new_patient_phone_number);
-        anchorPane2.getChildren().add(button_add_new_patient);
-        anchorPane2.getChildren().add(label_new_patient_sex);
-        anchorPane2.getChildren().add(label_new_patient_blood_group);
-        anchorPane2.getChildren().add(label_new_patient_id_type);
+        anchorPane_add_new_patient.getChildren().add(textField_new_patient_name);
+        anchorPane_add_new_patient.getChildren().add(textField_new_patient_surname);
+        anchorPane_add_new_patient.getChildren().add(datePicker_new_patient_birthday);
+        anchorPane_add_new_patient.getChildren().add(choiceBox_new_patient_sex);
+        anchorPane_add_new_patient.getChildren().add(choiceBox_new_patient_blood_group);
+        anchorPane_add_new_patient.getChildren().add(textField_new_patient_street);
+        anchorPane_add_new_patient.getChildren().add(textField_new_patient_house_number);
+        anchorPane_add_new_patient.getChildren().add(textField_new_patient_zip_code);
+        anchorPane_add_new_patient.getChildren().add(textField_new_patient_city);
+        anchorPane_add_new_patient.getChildren().add(choiceBox_new_patient_id_type);
+        anchorPane_add_new_patient.getChildren().add(textField_new_patient_id_number);
+        anchorPane_add_new_patient.getChildren().add(textField_new_patient_phone_number);
+        anchorPane_add_new_patient.getChildren().add(button_add_new_patient);
+        anchorPane_add_new_patient.getChildren().add(label_new_patient_sex);
+        anchorPane_add_new_patient.getChildren().add(label_new_patient_blood_group);
+        anchorPane_add_new_patient.getChildren().add(label_new_patient_id_type);
         tabPane_patients.getTabs().add(tab_add_new_patient);
         mainPane.getTabs().add(tab_patients);
     }
