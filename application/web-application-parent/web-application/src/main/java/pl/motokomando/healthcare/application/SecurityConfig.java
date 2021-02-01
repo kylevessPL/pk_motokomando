78a067be53b674e2091a4e50ca6864b441b43833
geekon <kacperpiasta@gmail.com>
2021-02-01 02:44:21 +0100

    add basic oauth endpoints security

---
+++ application/web-application-parent/web-application/src/main/java/pl/motokomando/healthcare/application/SecurityConfig.java
@@ -0,0 +1,26 @@
+package pl.motokomando.healthcare.application;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/v1/docs/**",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and().oauth2Login();
+    }
+
+}
