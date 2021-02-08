35cfb6a7aa7a3151ee15378e5fcf0153926a2ca0
geekon <kacperpiasta@gmail.com>
2021-02-08 22:03:28 +0100

    hotfix: intelliJ bad refactor

--- application/desktop-application-parent/desktop-application-controller/src/main/java/pl/motokomando/healthcare/controller/utils/WellKnownEndpoints.java
+++ application/desktop-application-parent/desktop-application-controller/src/main/java/pl/motokomando/healthcare/controller/utils/WellKnownEndpoints.java
@@ -2,6 +2,6 @@ package pl.motokomando.healthcare.controller.utils;
 
 public class WellKnownEndpoints {
 
-    public static final String USER_INFO = "/api/v1/authentication/userinfo";
+    public static final String USER_INFO = "/api/v1/authorization/userinfo";
 
 }
